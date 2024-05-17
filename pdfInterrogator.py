901fc650a3ad4a55e2f7d3a70f0b7ddc53393fd2
Matt Kotzbauer <mkotzbauer52@gmail.com>
2024-05-16 22:00:09 -0400

    init

--- pdfInterrogator.py
+++ pdfInterrogator.py
@@ -49,17 +49,45 @@ message_file = client.files.create(
   purpose = "assistants"
 )
 
-thread = client.beta.threads.create(
-  messages=[
-    {
-      "role": "user",
-      "content": "How many shares of AAPL were outstanding at the end of of October 2023?",
-      "attachments": [
-        { "file_id": message_file.id, "tools": [{"type": "file_search"}] }
-      ],
-    }
-  ]
-)
- 
+def run_prompt(prompt):
+  thread = client.beta.threads.create(
+    messages=[
+      {
+        "role": "user",
+        "content": prompt,
+        "attachments": [
+          { "file_id": message_file.id, "tools": [{"type": "file_search"}] }
+        ],
+      }
+    ]
+  )
+
+  run = client.beta.threads.runs.create_and_poll(
+      thread_id=thread.id, assistant_id=assistant.id
+  )
+
+  messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
+
+  message_content = messages[0].content[0].text
+  annotations = message_content.annotations
+  citations = []
+  for index, annotation in enumerate(annotations):
+      message_content.value = message_content.value.replace(annotation.text, f"[{index}]")
+      if file_citation := getattr(annotation, "file_citation", None):
+          cited_file = client.files.retrieve(file_citation.file_id)
+          citations.append(f"[{index}] {cited_file.filename}")
+
+  print(message_content.value)
+  print("\n".join(citations))
+
+
+prompts = [
+    "Summarize the content of the PDF",
+    "Summarize the content of pages 10 - 12"
+]
+
+for prompt in prompts:
+    run_prompt(prompt)
+
 
 
