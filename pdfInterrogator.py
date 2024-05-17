5f3d7e1ffbd8fd5f0513857afb8fc5e24677c6c5
Matt Kotzbauer <mkotzbauer52@gmail.com>
2024-05-16 22:01:28 -0400

    m

--- pdfInterrogator.py
+++ pdfInterrogator.py
@@ -53,7 +53,7 @@ thread = client.beta.threads.create(
   messages=[
     {
       "role": "user",
-      "content": "How many shares of AAPL were outstanding at the end of of October 2023?",
+      "content": "Summarize the content of the PDF",
       "attachments": [
         { "file_id": message_file.id, "tools": [{"type": "file_search"}] }
       ],
@@ -61,5 +61,20 @@ thread = client.beta.threads.create(
   ]
 )
  
+run = client.beta.threads.runs.create_and_poll(
+    thread_id=thread.id, assistant_id=assistant.id
+)
 
+messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
 
+message_content = messages[0].content[0].text
+annotations = message_content.annotations
+citations = []
+for index, annotation in enumerate(annotations):
+    message_content.value = message_content.value.replace(annotation.text, f"[{index}]")
+    if file_citation := getattr(annotation, "file_citation", None):
+        cited_file = client.files.retrieve(file_citation.file_id)
+        citations.append(f"[{index}] {cited_file.filename}")
+
+print(message_content.value)
+print("\n".join(citations))
